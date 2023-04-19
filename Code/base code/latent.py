c3f2060aeba3e93788a0cc0059b316968e7dfe7a
Karan Malik <42814324+Karan-Malik@users.noreply.github.com>
2023-04-18 17:40:23 -0700

    Added user based recommendation

--- Code/base code/latent.py
+++ Code/base code/latent.py
@@ -11,16 +11,16 @@ import numpy as np
 import movieposters as mp
 
 
-ratings_list = [i.strip().split("::") for i in open('data/ml-1m/ratings.dat', 'r').readlines()]
-users_list = [i.strip().split("::") for i in open('data/ml-1m/users.dat', 'r').readlines()]
-movies_list = [i.strip().split("::") for i in open('data/ml-1m/movies.dat', 'r',encoding='latin-1').readlines()]
+ratings_list2 = [i.strip().split("::") for i in open('data/ml-1m/ratings.dat', 'r').readlines()]
+users_list2 = [i.strip().split("::") for i in open('data/ml-1m/users.dat', 'r').readlines()]
+movies_list2 = [i.strip().split("::") for i in open('data/ml-1m/movies.dat', 'r',encoding='latin-1').readlines()]
 
-ratings_df = pd.DataFrame(ratings_list, columns = ['UserID', 'MovieID', 'Rating', 'Timestamp'], dtype = int)
-movies_df = pd.DataFrame(movies_list, columns = ['MovieID', 'Title', 'Genres'])
-movies_df['MovieID'] = movies_df['MovieID'].apply(pd.to_numeric)
+ratings_df2 = pd.DataFrame(ratings_list2, columns = ['UserID', 'MovieID', 'Rating', 'Timestamp'], dtype = int)
+movies_df2 = pd.DataFrame(movies_list2, columns = ['MovieID', 'Title', 'Genres'])
+movies_df2['MovieID'] = movies_df2['MovieID'].apply(pd.to_numeric)
 
 
-R_df = ratings_df.pivot(index = 'UserID', columns ='MovieID', values = 'Rating').fillna(0)
+R_df = ratings_df2.pivot(index = 'UserID', columns ='MovieID', values = 'Rating').fillna(0)
 R_df.head()
 
 R = R_df.to_numpy()
@@ -39,22 +39,22 @@ all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.re
 preds_df = pd.DataFrame(all_user_predicted_ratings, columns = R_df.columns)
 
 
-def recommend_movies(predictions_df, userID, movies_df, original_ratings_df, num_recommendations=5):
+def recommend_movies(preds_df, userID, movies_df2, ratings_df2, num_recommendations=5):
     
     # Get and sort the user's predictions
     user_row_number = userID - 1 # UserID starts at 1, not 0
-    sorted_user_predictions = predictions_df.iloc[user_row_number].sort_values(ascending=False)
+    sorted_user_predictions = preds_df.iloc[user_row_number].sort_values(ascending=False)
     #print(sorted_user_predictions)
     # Get the user's data and merge in the movie information.
-    user_data = original_ratings_df[original_ratings_df.UserID == (userID)]
-    user_full = (user_data.merge(movies_df, how = 'left', left_on = 'MovieID', right_on = 'MovieID').
+    user_data = ratings_df2[ratings_df2.UserID == (userID)]
+    user_full = (user_data.merge(movies_df2, how = 'left', left_on = 'MovieID', right_on = 'MovieID').
                      sort_values(['Rating'], ascending=False)
                  )
     
     temp=pd.DataFrame(sorted_user_predictions).reset_index()
     temp['MovieID']=temp['MovieID'].astype(int)
     
-    recommendations = (movies_df[~movies_df['MovieID'].isin(user_full['MovieID'])].
+    recommendations = (movies_df2[~movies_df2['MovieID'].isin(user_full['MovieID'])].
           merge(temp, how = 'left',
                 left_on = 'MovieID',
                 right_on = 'MovieID').
@@ -75,5 +75,5 @@ def recommend_movies(predictions_df, userID, movies_df, original_ratings_df, num
     
     return movie_name,year,genre,poster
 
-movie_name,year,genre,posters= recommend_movies(preds_df, 99, movies_df, ratings_df, 5)
+movie_name,year,genre,posters= recommend_movies(preds_df, 99, movies_df2, ratings_df2, 5)
 
--- Code/movie/routes.py
+++ Code/movie/routes.py
@@ -31,7 +31,35 @@ data_mean=matrix.mean()
 data_std=matrix.std()
 normalized_df=(matrix-matrix.mean())/matrix.std()
 
-movie_list=[]
+ratings_list2 = [i.strip().split("::") for i in open('data/ml-1m/ratings.dat', 'r').readlines()]
+users_list2 = [i.strip().split("::") for i in open('data/ml-1m/users.dat', 'r').readlines()]
+movies_list2 = [i.strip().split("::") for i in open('data/ml-1m/movies.dat', 'r',encoding='latin-1').readlines()]
+
+ratings_df2 = pd.DataFrame(ratings_list2, columns = ['UserID', 'MovieID', 'Rating', 'Timestamp'])
+movies_df2 = pd.DataFrame(movies_list2, columns = ['MovieID', 'Title', 'Genres'])
+movies_df2['MovieID'] = movies_df2['MovieID'].apply(pd.to_numeric)
+
+
+R_df = ratings_df2.pivot(index = 'UserID', columns ='MovieID', values = 'Rating').fillna(0)
+R_df.head()
+
+R = R_df.to_numpy()
+R=R.astype(int)
+user_ratings_mean = np.mean(R, axis = 1)
+R_demeaned = R - user_ratings_mean.reshape(-1, 1)
+
+
+from scipy.sparse.linalg import svds
+U, sigma, Vt = svds(R_demeaned, k = 50)
+
+sigma = np.diag(sigma)
+
+
+all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
+preds_df = pd.DataFrame(all_user_predicted_ratings, columns = R_df.columns)
+
+
+movie_list,year_list,genre_list=[],[],[]
 
 # copied from item_based end
 
@@ -42,7 +70,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/item_based',methods=['GET','POST'])
-def movierec():
+def itemBasedRec():
     form=MovieForm()
     
     if form.validate_on_submit():
@@ -72,7 +100,7 @@ def movierec():
             
             movie_name,year,poster=[],[],[]
             print('hello')
-            movie_list_item.clear()
+            movie_list.clear()
             for x in final_rec['Title'][1:]:
                 print('hello2',x)
                 name=x.split('(')[0][:-1]
@@ -94,6 +122,56 @@ def movierec():
                 
     return render_template('item_based.html',form=form)
 
+@app.route('/user_based',methods=['GET','POST'])
+def userBasedRec():
+    form=UserForm()
+    
+    if form.validate_on_submit():
+        m=form.moviename.data
+        m=int(m)
+# copy begin fuction samaan
+
+        user_row_number = m - 1 # UserID starts at 1, not 0
+        sorted_user_predictions = preds_df.iloc[user_row_number].sort_values(ascending=False)
+        #print(sorted_user_predictions)
+        # Get the user's data and merge in the movie information.
+        user_data = ratings_df2[ratings_df2.UserID == (m)]
+        user_full = (user_data.merge(movies_df2, how = 'left', left_on = 'MovieID', right_on = 'MovieID').
+                        sort_values(['Rating'], ascending=False)
+                    )
+        
+        temp=pd.DataFrame(sorted_user_predictions).reset_index()
+        temp['MovieID']=temp['MovieID'].astype(int)
+        
+        recommendations = (movies_df2[~movies_df2['MovieID'].isin(user_full['MovieID'])].
+            merge(temp, how = 'left',
+                    left_on = 'MovieID',
+                    right_on = 'MovieID').
+            rename(columns = {user_row_number: 'Predictions'}).
+            sort_values('Predictions', ascending = False).
+                            iloc[:5, :-1]
+                        )
+        movie_name,year,poster=[],[],[]
+        movie_list.clear()
+        for x in recommendations['Title']:
+            name=x.split('(')[0][:-1]
+            if name[-3:]=='The':
+                name=name[-3:]+' ' + name[:-5]
+            movie_list.append(name)
+            #poster.append(mp.get_poster(name))
+            #year.append(x.split('(')[1][:-1])
+        #genre=list(recommendations['Genres'])
+        return redirect(url_for('results'))
+
+                
+        #print(movie_name,year)
+
+# samaan ends item_based
+
+            #for i in range(1,5):
+            #    flash(final[i])
+                
+    return render_template('user_based.html',form=form)
 
 @app.route('/results',methods=['GET','POST'])
 def results():
